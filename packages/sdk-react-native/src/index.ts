9cd8becc91aaa29a55e083937b8f89cbbe23c56f
Khanh Le <58643031+nkgotcode@users.noreply.github.com>
2026-06-30 18:17:57 -0400

    Fix Android offline manifest signature verification

--- packages/sdk-react-native/src/index.ts
+++ packages/sdk-react-native/src/index.ts
@@ -175,6 +175,7 @@ export type OfflineManifest = {
   tickets: {
     ticketId: string;
     ticketTypeId: string;
+    eventOccurrenceId?: string;
     attendeeName: string;
     qrHash: string;
     status: string;
